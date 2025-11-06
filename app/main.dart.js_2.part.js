033d88a324b1ffb96db8305bb2d25bef83dc2314
IQUXAe <IQUXAe@users.noreply.github.com>
2025-11-06 15:50:15 +0300

    delete ai from web

--- app/main.dart.js_2.part.js
+++ app/main.dart.js_2.part.js
@@ -1,9 +1,9 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
-$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={p2:function p2(){}}
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={p_:function p_(){}}
 B=c[0]
 A=a.updateHolder(c[5],A)
-A.p2.prototype={
+A.p_.prototype={
 D(d,e){return this.gmv(this).$1(e)}}
 var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
-y(A.p2,B.E)})()};
-(a=>{a["co6G0lyk47/N3VWRRcDvxmoNia4="]=a.current})($__dart_deferred_initializers__);
+y(A.p_,B.E)})()};
+(a=>{a["qezsuYrEqYaFLaqn/AHtooETrBU="]=a.current})($__dart_deferred_initializers__);
