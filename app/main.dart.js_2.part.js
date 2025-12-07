04e875a079e45a3d8726b80319aa38f930f68756
IQUXAe <iquxae@example.com>
2025-12-07 14:03:36 +0300

    update web app

--- app/main.dart.js_2.part.js
+++
@@ -1,9 +0,0 @@
-((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
-$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={p_:function p_(){}}
-B=c[0]
-A=a.updateHolder(c[5],A)
-A.p_.prototype={
-D(d,e){return this.gmv(this).$1(e)}}
-var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
-y(A.p_,B.E)})()};
-(a=>{a["qezsuYrEqYaFLaqn/AHtooETrBU="]=a.current})($__dart_deferred_initializers__);
---
+++ web/main.dart.js_2.part.js
@@ -0,0 +1,9 @@
+((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={oZ:function oZ(){}}
+B=c[0]
+A=a.updateHolder(c[5],A)
+A.oZ.prototype={
+E(d,e){return this.gmv(this).$1(e)}}
+var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
+y(A.oZ,B.E)})()};
+(a=>{a["BztliMeYRomjUU0EeC8RPDMyExI="]=a.current})($__dart_deferred_initializers__);
