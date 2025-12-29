9918db1c310d78ebbbd0acb02b4cfa7ff1e2c8a0
IQUXAe <iquxae@example.com>
2025-12-29 17:17:52 +0300

    update web app

--- web/main.dart.js_2.part.js
+++ web/main.dart.js_2.part.js
@@ -1,9 +1,9 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
-$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={pf:function pf(){}}
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={ky:function ky(){}}
 B=c[0]
-A=a.updateHolder(c[5],A)
-A.pf.prototype={
-E(d,e){return this.gmG(this).$1(e)}}
+A=a.updateHolder(c[7],A)
+A.ky.prototype={
+E(d,e){return this.gkC(this).$1(e)}}
 var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
-y(A.pf,B.E)})()};
-(a=>{a["LJlw4JGjirbLNFNtYbQbwTbBHmY="]=a.current})($__dart_deferred_initializers__);
+y(A.ky,B.D)})()};
+(a=>{a["DuJEgK+2mB1nGGmDwqz+9AblgA4="]=a.current})($__dart_deferred_initializers__);
